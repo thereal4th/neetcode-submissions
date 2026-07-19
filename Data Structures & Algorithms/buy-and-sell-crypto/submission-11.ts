46696e39941f9c77f2e13c2e4b3411d4899a412f
Alfredo Venturina <alfredoventurina@gmail.com>
2026-07-20 06:53:49 +0800

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.ts
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+
+        //two pointer pattern
+        let left = 0;
+        let right = prices.length - 1;
+
+        let buy = {
+            price: prices[left],
+            index: left,
+        }
+        let sell = {
+            price: prices[right],
+            index: right,
+        }
+
+        while(left < prices.length){
+
+            if(prices[left] < buy.price && left < sell.index){
+                buy.price = prices[left];
+                buy.index = left;
+            }
+            if(prices[right] > sell.price && buy.index < right){
+                sell.price = prices[right];
+                sell.index = right;
+            }
+
+            left++;
+            right--;
+        }
+
+        const profit = sell.price - buy.price;
+        
+        if(profit <= 0){
+            return 0
+        }
+        else{
+            return profit;
+        }
+
+    }
+}
